f7527a027c98198900523fd1120fbee2076f87b2
jtan <jtan@PL-PH-PF2DKQD3.planit.local>
2023-10-17 14:56:57 +1100

    Change verification in Login

--- Scripts/TC03_Login/Script1697426558820.groovy
+++ Scripts/TC03_Login/Script1697426558820.groovy
@@ -1,5 +1,5 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 WebUI.openBrowser('')
@@ -19,18 +19,17 @@ WebUI.click(findTestObject('Homepage/Page_JupiterPrime/input__agree'))
 
 WebUI.click(findTestObject('Homepage/Page_JupiterPrime/button_Log In'))
 
-WebUI.waitForElementPresent(findTestObject('Homepage/Page_JupiterPrime/span_Hi Test'), 0)
+WebUI.verifyElementPresent(findTestObject('Homepage/Page_JupiterPrime/span_Hi Test'), 0)
 
 String expectedUsername = 'Hi Test'
 
 String actualUsername = WebUI.getText(findTestObject('Homepage/Page_JupiterPrime/span_Hi Test'))
 
 if (actualUsername == expectedUsername) {
-  //  println('User is logged in as expected user: ' + actualUsername)
-	KeywordUtil.logInfo('User is logged in as expected user: ' + actualUsername)
+    //  println('User is logged in as expected user: ' + actualUsername)
+    KeywordUtil.logInfo('User is logged in as expected user: ' + actualUsername) //   println('User is not logged in as the expected user. Actual user: ' + actualUsername)
 } else {
- //   println('User is not logged in as the expected user. Actual user: ' + actualUsername)
-	KeywordUtil.logInfo('User is not logged in as expected user: ' + actualUsername)
+    KeywordUtil.logInfo('User is not logged in as expected user: ' + actualUsername)
 }
 
 WebUI.closeBrowser()
